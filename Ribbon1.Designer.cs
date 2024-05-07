be19c31175c9a0e7cdc8d8f981860f8c63e46a1d
Ajitesh Taneja <ataneja789@gmail.com>
2024-05-06 17:28:54 -0700

    Add project files.

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Drop_VC
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl1 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl2 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl3 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl4 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl5 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl6 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl7 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl8 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl9 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl10 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl11 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl12 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl13 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl14 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl15 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl16 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl17 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl18 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl19 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl20 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl21 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl22 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl23 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl24 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl25 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl26 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl27 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl28 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl29 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl30 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl31 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl32 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl33 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl34 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl35 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl36 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl37 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl38 = this.Factory.CreateRibbonDropDownItem();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.identifiers = this.Factory.CreateRibbonGroup();
+            this.titleFont = this.Factory.CreateRibbonEditBox();
+            this.titleSize = this.Factory.CreateRibbonDropDown();
+            this.divider = this.Factory.CreateRibbonCheckBox();
+            this.main1 = this.Factory.CreateRibbonGroup();
+            this.drop = this.Factory.CreateRibbonButton();
+            this.rmv = this.Factory.CreateRibbonButton();
+            this.settings = this.Factory.CreateRibbonGroup();
+            this.fontDrop = this.Factory.CreateRibbonEditBox();
+            this.lineDrop = this.Factory.CreateRibbonDropDown();
+            this.margin = this.Factory.CreateRibbonCheckBox();
+            this.tab1.SuspendLayout();
+            this.identifiers.SuspendLayout();
+            this.main1.SuspendLayout();
+            this.settings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.identifiers);
+            this.tab1.Groups.Add(this.main1);
+            this.tab1.Groups.Add(this.settings);
+            this.tab1.Label = "Drop Cap";
+            this.tab1.Name = "tab1";
+            // 
+            // identifiers
+            // 
+            this.identifiers.Items.Add(this.titleFont);
+            this.identifiers.Items.Add(this.titleSize);
+            this.identifiers.Items.Add(this.divider);
+            this.identifiers.Label = "Title Identifiers";
+            this.identifiers.Name = "identifiers";
+            // 
+            // titleFont
+            // 
+            this.titleFont.Label = "Font";
+            this.titleFont.Name = "titleFont";
+            this.titleFont.ScreenTip = "Title\'s Font will have to contain these characters";
+            this.titleFont.Text = null;
+            this.titleFont.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.titleFont_Change);
+            // 
+            // titleSize
+            // 
+            ribbonDropDownItemImpl1.Label = "1";
+            ribbonDropDownItemImpl2.Label = "2";
+            ribbonDropDownItemImpl3.Label = "3";
+            ribbonDropDownItemImpl4.Label = "4";
+            ribbonDropDownItemImpl5.Label = "5";
+            ribbonDropDownItemImpl6.Label = "6";
+            ribbonDropDownItemImpl7.Label = "7";
+            ribbonDropDownItemImpl8.Label = "8";
+            ribbonDropDownItemImpl9.Label = "9";
+            ribbonDropDownItemImpl10.Label = "10";
+            ribbonDropDownItemImpl11.Label = "11";
+            ribbonDropDownItemImpl12.Label = "12";
+            ribbonDropDownItemImpl13.Label = "13";
+            ribbonDropDownItemImpl14.Label = "14";
+            ribbonDropDownItemImpl15.Label = "15";
+            ribbonDropDownItemImpl16.Label = "16";
+            ribbonDropDownItemImpl17.Label = "17";
+            ribbonDropDownItemImpl18.Label = "18";
+            ribbonDropDownItemImpl19.Label = "19";
+            ribbonDropDownItemImpl20.Label = "20";
+            ribbonDropDownItemImpl21.Label = "22";
+            ribbonDropDownItemImpl22.Label = "24";
+            ribbonDropDownItemImpl23.Label = "26";
+            ribbonDropDownItemImpl24.Label = "28";
+            ribbonDropDownItemImpl25.Label = "30";
+            ribbonDropDownItemImpl26.Label = "35";
+            ribbonDropDownItemImpl27.Label = "40";
+            ribbonDropDownItemImpl28.Label = "45";
+            ribbonDropDownItemImpl29.Label = "50";
+            ribbonDropDownItemImpl30.Label = "55";
+            ribbonDropDownItemImpl31.Label = "60";
+            this.titleSize.Items.Add(ribbonDropDownItemImpl1);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl2);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl3);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl4);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl5);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl6);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl7);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl8);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl9);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl10);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl11);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl12);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl13);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl14);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl15);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl16);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl17);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl18);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl19);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl20);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl21);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl22);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl23);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl24);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl25);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl26);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl27);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl28);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl29);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl30);
+            this.titleSize.Items.Add(ribbonDropDownItemImpl31);
+            this.titleSize.Label = "Size";
+            this.titleSize.Name = "titleSize";
+            this.titleSize.ScreenTip = "Fonts have to be this large to be considered a Title";
+            this.titleSize.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.titleSize_change);
+            // 
+            // divider
+            // 
+            this.divider.Label = "Divider";
+            this.divider.Name = "divider";
+            this.divider.ScreenTip = "Check this box if below each Title there is a one Paragraph Divider";
+            this.divider.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.divFunc);
+            // 
+            // main1
+            // 
+            this.main1.Items.Add(this.drop);
+            this.main1.Items.Add(this.rmv);
+            this.main1.Label = "Main";
+            this.main1.Name = "main1";
+            // 
+            // drop
+            // 
+            this.drop.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.drop.Image = global::Drop_VC.Properties.Resources.ico;
+            this.drop.Label = "Auto Drop Cap";
+            this.drop.Name = "drop";
+            this.drop.ScreenTip = "Drop Cap\'s all Paragraphs that are below a title identified by the Identifiers";
+            this.drop.ShowImage = true;
+            this.drop.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.drop_Click);
+            // 
+            // rmv
+            // 
+            this.rmv.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.rmv.Image = global::Drop_VC.Properties.Resources.rmcico;
+            this.rmv.Label = "Remove Drop Cap";
+            this.rmv.Name = "rmv";
+            this.rmv.ScreenTip = "Removes all Drop Cap in the Document";
+            this.rmv.ShowImage = true;
+            this.rmv.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.rmv_Click);
+            // 
+            // settings
+            // 
+            this.settings.Items.Add(this.fontDrop);
+            this.settings.Items.Add(this.lineDrop);
+            this.settings.Items.Add(this.margin);
+            this.settings.Label = "Drop Settings";
+            this.settings.Name = "settings";
+            // 
+            // fontDrop
+            // 
+            this.fontDrop.Label = "Font";
+            this.fontDrop.Name = "fontDrop";
+            this.fontDrop.ScreenTip = "Drop Cap Font, leave blank for same font as other text.\nIf you want a different f" +
+    "ont, copy paste it as it is.";
+            this.fontDrop.Text = null;
+            this.fontDrop.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.font_Drop_Change);
+            // 
+            // lineDrop
+            // 
+            ribbonDropDownItemImpl32.Label = "1";
+            ribbonDropDownItemImpl33.Label = "2";
+            ribbonDropDownItemImpl34.Label = "3";
+            ribbonDropDownItemImpl35.Label = "4";
+            ribbonDropDownItemImpl36.Label = "5";
+            ribbonDropDownItemImpl37.Label = "6";
+            ribbonDropDownItemImpl38.Label = "7";
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl32);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl33);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl34);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl35);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl36);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl37);
+            this.lineDrop.Items.Add(ribbonDropDownItemImpl38);
+            this.lineDrop.Label = "Lines To Drop";
+            this.lineDrop.Name = "lineDrop";
+            this.lineDrop.ScreenTip = "Drop Cap Setting";
+            this.lineDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.lineDrop_Change);
+            // 
+            // margin
+            // 
+            this.margin.Label = "In Margin";
+            this.margin.Name = "margin";
+            this.margin.ScreenTip = "Check to put Dropped Character in Margin";
+            this.margin.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.margin_Change);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.identifiers.ResumeLayout(false);
+            this.identifiers.PerformLayout();
+            this.main1.ResumeLayout(false);
+            this.main1.PerformLayout();
+            this.settings.ResumeLayout(false);
+            this.settings.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup main1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton drop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton rmv;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup identifiers;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup settings;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown titleSize;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox divider;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox titleFont;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox fontDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown lineDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox margin;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,232 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Tools.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Drop_VC
+{
+    public partial class Ribbon1
+    {
+
+        string content = "table of content";
+        int cutOff = 13;
+        string fontReq = "";
+        bool div = false;
+        int lineDropped = 3;
+        int wDmargin = 1;
+        string fontDropped = "";
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            titleFont.Text = "";
+            titleSize.SelectedItemIndex = 12;
+            divider.Checked = false;
+            fontDrop.Text = "";
+            lineDrop.SelectedItemIndex = 2;
+            margin.Checked = false;
+        }
+
+        // Main drop function
+        private void drop_Click(object sender, RibbonControlEventArgs e)
+        {
+            //MessageBox.Show("Hello");
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            Word.Paragraphs paras = doc.Paragraphs;
+
+            int i = 1, skip;
+            bool drop = false;
+            string txt, rep;
+
+            while (true)
+            {
+                if (paras[i].Range.Text.Length > 1)
+                {
+                    if (isTitle(paras[i]))
+                    {
+                        /*
+                        if (paras[i].Range.Text.Length < 20)
+                        {
+                            MessageBox.Show("Found a title, Text:" + paras[i].Range.Text.Substring(0, paras[i].Range.Text.Length));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Found a title, Text:" + paras[i].Range.Text.Substring(0, 20));
+                        }
+                        */
+
+                        drop = true;
+                        if (div) i++;
+
+                    }
+                    else if (drop)
+                    {
+                        //MessageBox.Show("Found a paragraph to drop, Text: " + paras[i].Range.Text.Substring(0, 20));
+                        skip = specialChars(paras[i]);
+
+                        txt = paras[i].Range.Text;
+                        paras[i].Range.Text = txt.Substring(skip);
+                        dropTxt(paras[i]);
+                        rep = txt.Substring(0, skip + 1);
+                        paras[i].Range.Text = rep;
+
+                        drop = false;
+                        i++;
+                    }
+                }
+
+                if (i == doc.Paragraphs.Count) break;
+                i++;
+            }
+
+            /*
+                string txt = para.Range.Text;
+                string temp = txt.Substring(1);
+                para.Range.Text = temp;
+                char x = txt.ToCharArray()[0];
+                MessageBox.Show("Removed first char: " + x + " in int form " + (int)x);
+
+                Word.DropCap dropCap = paras[1].DropCap;
+                dropCap.Position = Word.WdDropPosition.wdDropNormal;
+                dropCap.LinesToDrop = 3;
+                dropCap.DistanceFromText = 1;
+                dropCap.Enable();
+
+                MessageBox.Show("Dropped");
+
+                string rep = txt.Substring(0,2);
+                paras[1].Range.Text = rep;
+            */
+
+            //MessageBox.Show("Done?");
+        }
+
+
+        // Getting number of non alphabetic characters from the front of the paragraph
+        private int specialChars(Word.Paragraph para)
+        {
+            char[] txt = para.Range.Text.ToCharArray();
+
+            int i = 0, temp;
+            while (true)
+            {
+                temp = txt[i];
+                if (temp > 90) temp -= 32;
+                //MessageBox.Show("i: " + i + " Char: " + txt[i] + " Int: " + (int)txt[i] + " Used Int: " + temp);
+                if (temp >= 65 && temp <= 90) break;
+                i++;
+            }
+            return i;
+        }
+
+        // Is checking if the paragraph is a title
+        private bool isTitle(Word.Paragraph para)
+        {
+
+            string txt = para.Range.Text;
+            
+            if (txt.ToLower().Contains(content)) return false;// Is table of content
+
+            int size = (int) para.Range.Font.Size;
+
+            if (fontReq != "")
+            {
+                if (para.Range.Font.Name.Contains(fontReq))
+                {
+                    return (size >= cutOff);
+                }
+                else return false;
+            } else if (size >= cutOff) return true;
+
+            //MessageBox.Show("Size is " + size + " Cutoff is " + cutOff);
+            return false;
+        }
+
+
+
+        // Does the Drop Cap
+        private void dropTxt(Word.Paragraph para)
+        {
+            Word.DropCap dropCap = para.DropCap;
+            if (fontDropped != "") dropCap.FontName = fontDropped;
+            dropCap.Position = (Word.WdDropPosition) wDmargin;
+            dropCap.LinesToDrop = lineDropped;
+            dropCap.DistanceFromText = 1;
+            dropCap.Enable();
+        }
+
+
+
+        // Function to remove all drop cap, doesn't need to check just do it on all.
+        private void rmv_Click(object sender, RibbonControlEventArgs e)
+        {
+            //MessageBox.Show("Remove");
+
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            Word.Paragraphs paras = doc.Paragraphs;
+            Word.DropCap dropCap;
+
+            int i = 1;
+            while (true)
+            {
+                // At 1 it does it for all page number and that breaks as they cannot have dropCap
+                if (paras[i].Range.Text.Length > 10)
+                {
+                    //MessageBox.Show("Paragraph " + i + ". With length " + paras[i].Range.Text.Length);
+                    dropCap = paras[i].DropCap;
+                    if (dropCap != null) dropCap.Clear();
+                }
+                if (i == doc.Paragraphs.Count) break;
+                i++;
+            }
+
+        }
+
+
+        // Title function
+        private void titleFont_Change(object sender, RibbonControlEventArgs e)
+        {
+            RibbonEditBox obj = (RibbonEditBox) sender;
+            fontReq = obj.Text;
+        }
+
+        private void titleSize_change(object sender, RibbonControlEventArgs e)
+        {
+            RibbonDropDown obj = (RibbonDropDown) sender;
+            cutOff = int.Parse(obj.SelectedItem.Label);
+        }
+
+        private void divFunc(object sender, RibbonControlEventArgs e)
+        {
+            RibbonCheckBox obj = (RibbonCheckBox) sender;
+            div = obj.Checked;
+        }
+
+
+
+
+        // Drop Cap functions
+        private void font_Drop_Change(object sender, RibbonControlEventArgs e)
+        {
+            RibbonEditBox obj = (RibbonEditBox) sender;
+            fontDropped = obj.Text;
+        }
+
+        private void lineDrop_Change(object sender, RibbonControlEventArgs e)
+        {
+            RibbonDropDown obj = (RibbonDropDown) sender;
+            lineDropped = int.Parse(obj.SelectedItem.Label);
+        }
+
+        private void margin_Change(object sender, RibbonControlEventArgs e)
+        {
+            RibbonCheckBox obj = (RibbonCheckBox) sender;
+            if (obj.Checked) wDmargin = 2;
+            else wDmargin = 1;
+        }
+    }
+}
